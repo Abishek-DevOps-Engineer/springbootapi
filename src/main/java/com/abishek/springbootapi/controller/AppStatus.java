1b8594d1ad610ae5cc95b37d0b5eced9b93d8d3c
Abishek-DevOps-Engineer <abishekmessi123@gmail.com>
2026-04-26 15:33:15 +0530

    Appication status api

---
+++ src/main/java/com/abishek/springbootapi/controller/AppStatus.java
@@ -0,0 +1,13 @@
+package com.abishek.springbootapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppStatus {
+
+    @GetMapping("/appStatus")
+    public String getAppStstus(){
+        return "Application running Successfully";
+    }
+}
